31f58e8838797e9e7199489833873e9f8e2f7e9c
michelle ho <12551177+michellemho@users.noreply.github.com>
2021-02-26 17:38:47 -0800

    one pass answer, with assist from geoff!

--- leetcode/121-best-time-to-buy-and-sell-stock.py
+++ leetcode/121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profits = []
-        for buy_time, buy_price in enumerate(prices):
-            for sell_price in prices[buy_time:]:
-                profits.append(sell_price - buy_price)
-        return max(profits)
+        max_point = 0
+        min_point = 0
+        curr_max_profit = 0
+        for time_point, value in enumerate(prices):
+            if value < prices[min_point]:
+                min_point = time_point
+            if max_point < min_point:
+                max_point = time_point
+            if value > prices[max_point]:
+                max_point = time_point
+            if curr_max_profit < prices[max_point] - prices[min_point]:
+                curr_max_profit = prices[max_point] - prices[min_point]
+        return curr_max_profit
